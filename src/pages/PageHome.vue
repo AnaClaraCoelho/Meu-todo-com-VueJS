690cdd1ff84bdbe8bc32c0ffb875468b3f8fb766
Seu Nome <voce@exemplo.com>
2022-09-08 15:05:39 -0300

    "Conexão coma API do lista de tarefas"

---
+++ src/pages/PageHome.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <TarefaList msg="Welcome to Your Vue.js PageHome" :tasks="listadeTarefa" />
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+import TarefaList from "../components/TarefaList.vue";
+
+export default {
+  components: {
+    TarefaList,
+  },
+  data: () => {
+    return {
+      listadeTarefa: ["A", "B", "C"],
+    };
+  },
+  created() {
+    console.log("terminei de carregar a página!");
+    axios.get("http://localhost:3000/tasks/").then((response) => {
+      console.log(response.data);
+      this.listadeTarefa = response.data;
+    });
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
